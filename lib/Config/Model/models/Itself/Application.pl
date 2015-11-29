b4580e53d1f488947454e1f058c0056b7b632b1e
Dominique Dumont <dod@debian.org>
2015-11-29 20:48:26 +0100

    reformat Application.pl

--- lib/Config/Model/models/Itself/Application.pl
+++ lib/Config/Model/models/Itself/Application.pl
@@ -1,39 +1,35 @@
 
 [
-  {
-    'name' => 'Itself::Application',
-    # read/written by Config::Model::Itself (read_all)
-    'element' => [
-      'model',
-      {
-        'refer_to' => '! class',
-        'type' => 'leaf',
-        'value_type' => 'reference'
-      },
-      'category',
-      {
-        'choice' => [
-          'system',
-          'user',
-          'application'
+    {
+        name => 'Itself::Application',
+        # read/written by Config::Model::Itself (read_all)
+
+        element => [
+            model => {
+                refer_to => '! class',
+                type => 'leaf',
+                value_type => 'reference'
+            },
+            category => {
+                choice => [
+                    'system',
+                    'user',
+                    'application'
+                ],
+                type => 'leaf',
+                value_type => 'enum'
+            },
+            allow_config_file_override => {
+                type => 'leaf',
+                upstream_default => '0',
+                value_type => 'boolean'
+            },
+            require_config_file => {
+                type => 'leaf',
+                upstream_default => '0',
+                value_type => 'boolean'
+            }
         ],
-        'type' => 'leaf',
-        'value_type' => 'enum'
-      },
-      'allow_config_file_override',
-      {
-        'type' => 'leaf',
-        'upstream_default' => '0',
-        'value_type' => 'boolean'
-      },
-      'require_config_file',
-      {
-        'type' => 'leaf',
-        'upstream_default' => '0',
-        'value_type' => 'boolean'
-      }
-    ],
-  }
-]
-;
+    }
+] ;
 
