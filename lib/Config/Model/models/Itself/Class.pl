b3898f541d7c96414667ac70378a8eebe66a9d6b
Dominique Dumont <dod@debian.org>
2017-05-16 20:50:23 +0200

    fix yaml_class param to show up with yaml backend
    used to show up with IniFile backend which is a bit of a bummer

--- lib/Config/Model/models/Itself/Class.pl
+++ lib/Config/Model/models/Itself/Class.pl
@@ -228,7 +228,7 @@
                 upstream_default => 'YAML::Tiny',
                 warp             => {
                     follow => '- backend',
-                    rules  => [ IniFile => { level => 'normal', } ],
+                    rules  => [ Yaml => { level => 'normal', } ],
                 }
             },
 
