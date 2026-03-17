dod38fr/config-model-itself

commit 5776e983215ee0893b4376c1caf9b21f31dc7db3

BackendDetector: replace File::Find with Path::Tiny

lib/Config/Model/Itself/BackendDetector.pm
@@ -10,7 +10,7 @@ use feature qw/postderef signatures/;
 no warnings qw/experimental::postderef experimental::signatures/;
 
 use Pod::POM ;
-use File::Find ;
+use Path::Tiny;
 
 use base qw/Config::Model::Value/ ;
 
@@ -22,18 +22,17 @@ sub setup_enum_choice ($self, @args) {
     my %choices = map { ($_ => 1);} ref $args[0] ? @{$args[0]} : @args ;
 
     # find available backends in all @INC directories
-    my $wanted = sub { 
-        my $n = $File::Find::name ;
-        if (-f $_ and $n =~ s/\.pm$// and $n !~ /Any$/) {
-	    $n =~ s!.*Backend/!! ;
-	    $n =~ s!/!::!g ;
-	    $choices{$n} = 1 ;
+    my $wanted = sub ($path, $) {
+        my $n = $path->basename();
+        if ($path->is_file and $n =~ s/\.pm$// and $n !~ /Any$/) {
+            $n =~ s!/!::!g ;
+            $choices{$n} = 1 ;
         }
     } ;
 
     foreach my $inc (@INC) {
-        my $path = "$inc/Config/Model/Backend" ;
-        find ($wanted, $path ) if -d $path;
+        my $path = path($inc)->child("Config/Model/Backend") ;
+        $path->visit($wanted, { recurse => 1} ) if $path->is_dir;
     }
 
     $self->SUPER::setup_enum_choice(sort keys %choices) ;
@@ -50,31 +49,31 @@ sub set_help {
 
     my $parser = Pod::POM->new();
 
-    my $wanted = sub { 
-        my $n = $File::Find::name ;
-
-        return unless (-f $n and $n !~ /Any\.pm$/) ;
-        my $file = $n ;
-        $n =~ s/\.pm$//;
-        $n =~ s!/!::!g ;
-        my $perl_name = $n ;
-        $n =~ s!.*Backend::!! ;
+    my $wanted = sub ($path, $) {
+        return unless ($path->is_file);
+        return if $path->basename eq "Any.pm" ;
+        return if $path =~ /Role/;
+        my $help_key = $path->stringify;
+        $help_key =~ s/\.pm$//;
+        $help_key =~ s!/!::!g ;
+        my $perl_name = $help_key ;
+        $help_key =~ s!.*Backend::!! ;
         $perl_name =~ s!.*Config!Config! ;
 
-        my $pom = $parser->parse_file($file)|| die $parser->error();
+        my $pom = $parser->parse_file($path->stringify) || die $parser->error();
 
         foreach my $head1 ($pom->head1()) {
             if ($head1->title() eq 'NAME') {
                 my $c = $head1->content();
                 $c =~ s/.*?-\s*//;
                 $c =~ s/\n//g;
-                $help->{$n} = $c . " provided by L<$perl_name>";
+                $help->{$help_key} = $c . " provided by L<$perl_name>";
                 last;
             }
         }
     };
 
-    find ($wanted, $path ) ;
+    path($path)->visit ($wanted, {recurse => 1} ) ;
 
     $self->{help} =  $help;
     return;

t/backend_detect.t
@@ -40,6 +40,7 @@ my @choices = $backend->get_choice ;
 
 ok( (scalar grep { $_ eq 'IniFile'} @choices), "IniFile plugin backend was found") ;
 
+# test help from backends provided by Config::Model
 SKIP: {
     skip "this help is available after Config::Model is built", 1 unless
         $Config::Model::VERSION < 3;
@@ -51,6 +52,7 @@ SKIP: {
          "Found IniFile doc in pod") ;
 }
 
+# test help for backend provided by Master class above
 my $help = $backend->get_help('cds_file') ;
 is($help,"file ...", "cds_file help was kept") ;
 

commit de23c0aeee65a1fc5cd8fe12a9d7b7452514cb6a

fix backend detection
Commit 5776e983215ee0893b4376c1caf9b21f31dc7db3 broke backend
detection. The backend Dpkg::Control was no longer found, instead
only Control backend was found.

data/Backend/Master/Other.pm
@@ -1,0 +1,2 @@
+package Config::Model::Backend::Master::Other;
+1;

data/Backend/MasterModel.pm
@@ -1,0 +1,2 @@
+package Config::Model::Backend::MasterModel;
+1;

lib/Config/Model/Itself/BackendDetector.pm
@@ -23,7 +23,9 @@ sub setup_enum_choice ($self, @args) {
 
     # find available backends in all @INC directories
     my $wanted = sub ($path, $) {
-        my $n = $path->basename();
+        # Get the backend file relative to Config/Model/Backend dir
+        my ($n) = ($path =~ s!.*Backend/!!r);
+
         if ($path->is_file and $n =~ s/\.pm$// and $n !~ /Any$/) {
             $n =~ s!/!::!g ;
             $choices{$n} = 1 ;

t/backend_detect.t
@@ -2,14 +2,26 @@
 
 use ExtUtils::testlib;
 use Test::More ;
+use File::Copy::Recursive qw(fcopy rcopy dircopy);
 use Config::Model;
-use Config::Model::Tester::Setup qw/init_test/;
+use Config::Model::Tester::Setup qw/init_test setup_test_dir/;
 use Test::Memory::Cycle;
 
 use warnings;
 use strict;
 
 my ($model, $trace) = init_test();
+
+# do search for the models created in this test
+use lib "wr_root/backend_detect/lib";
+
+my $wr_test = setup_test_dir ;
+my $wr_lib = $wr_test->child("lib/Config/Model");
+
+$wr_lib->mkpath;
+
+# copy test model
+dircopy('data',$wr_lib->stringify) || die "cannot copy model data:$!" ;
 
 $model ->create_config_class (
    name => "Master",
@@ -38,7 +50,9 @@ my $backend = $root->fetch_element('backend') ;
 
 my @choices = $backend->get_choice ;
 
-ok( (scalar grep { $_ eq 'IniFile'} @choices), "IniFile plugin backend was found") ;
+ok( (scalar grep { $_ eq 'IniFile'} @choices), "IniFile plugin backend from Config::Model was found") ;
+ok( (scalar grep { $_ eq 'MasterModel'} @choices), "MasterModel plugin backend from test files was found") ;
+ok( (scalar grep { $_ eq 'Master::Other'} @choices), "MasterModel::Other plugin backend from test files was found") ;
 
 # test help from backends provided by Config::Model
 SKIP: {